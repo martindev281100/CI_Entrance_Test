d3d009d2e1722b4c4b8a43a9a41c7382ffd19b44
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-07-29 16:16:08 +0700

    new commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// // Exercise 1
+// let n = Number(prompt('Enter n: '));
+// let arr_n = [];
+// for (let i = 0; i< n; i++)
+// {
+//     arr_n.push(i);
+// }
+// console.log(arr_n)
+//
+// let opposite_num = Math.floor(n/2);
+// console.log(opposite_num)
+
+// //Exercise 2
+// let s1 = 'super'
+// let s2 = 'tower'
+// let split1 = s1.split('')
+// let split2 = s2.split('')
+// let new_arr= []
+// for(let i = 0; i<split1.length; i++)
+// {
+//     new_arr.push(split1[i]);
+//     new_arr.push(split2[i]);
+// }
+// new_str = new_arr.toString()
+// console.log(new_str)
+
